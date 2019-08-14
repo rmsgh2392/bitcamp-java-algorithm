b8bbf2471e969b45310a91bd3911a6d17ff266c3
rmsgh2392 <rmsgh2392@daum.net>
2019-08-14 20:53:24 +0900

    190814 20:50

--- homework02/BMI.java
+++ homework02/BMI.java
@@ -1,10 +1,54 @@
 package homework02;
 
+import java.util.Scanner;
+
+/**사람의 이름을 입력하고, 몸무게 , 키를 입력하면
+해당 사람의 상태가 비만인지 저체중인지 알려주는 BMI 지수 측정하는 프로그램을 만들어 주세요.
+BMI 구하는 공식은 아래와 같아요. 더 궁금하면 여기를 클릭하세요.
+       BMI = x KG / (y M * y M)
+
+Example for 175 cm height and 70 kg weight:
+BMI = 70 / (1.75 * 1.75) = 22.86
+
+(bmi > 30.0) 고도비만
+(bmi > 25.0) 비만
+(bmi > 23.0) 과체중
+(bmi > 18.5) 정상
+저체중
+       이랍니다.
+*/
 public class BMI {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		String name = "";
+		double height =0, BMI = 0;
+		int weight = 0;
+		System.out.println("당신의 이름을 입력해주세요 :");
+		name = scanner.next();
+		System.out.println("몸무게는 얼마입니까 ?");
+		weight = scanner.nextInt();		
+		System.out.println("키는 몇 cm입니까 ?");
+		height =scanner.nextDouble();
+		System.out.print("BMI는 "+BMI);
+		BMI = weight /(height * height)*10/10;
+		
+		
+		if(BMI > 30.0){
+			System.out.println("고도비만입니다 당장 살 뺴세요.");
+		}else if(BMI > 25.0) {
+			System.out.println("당신은 비만입니다 위험합니다.");
+		}else if(BMI >23.0){
+			System.out.println("과체중입니다 조절하세요");
+		}else if(BMI >18.5){
+			System.out.println("정상이세요");
+		}else {
+			System.out.println("저체중입니다 많이 드세요 ");
+		}
+		
+		
+		
+		
 	}
 
 }
--- homework02/Ranking.java
+++ homework02/Ranking.java
@@ -1,10 +1,69 @@
 package homework02;
 
+import java.util.Scanner;
+
+/**
+ * To. 개발자님
+저희는 비트올림픽 위원회입니다.
+한번의 트랙에서 육상선수는 총 3명 입니다.
+총 세명의 100m 기록을 입력받아서 
+1, 2, 3 등을 가려야 합니다.
+
+A 선수 기록 : 10.5초
+B 선수 기록 : 10.8초
+C 선수 기록 : 10.1초 
+라고 입력받았다면
+
+###### 성적 랭킹 #######
+
+1등 : C 선수 기록 (10.1초)
+2등 : A 선수 기록 (10.5초)
+3등 : B 선수 기록 (10.8초)
+
+라고 출력시켜주세요.
+
+ * @author user
+ *
+ */
 public class Ranking {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		String[] player = {"kor", "usa" ,"amc"};
+		double rpt1,rpt2,rpt3 = 0;
+		System.out.println("3명 선수의 100미터 기록");
+		System.out.printf("%s선수의 기록 :\n",player[0]);
+		rpt1 =scanner.nextDouble();
+		System.out.printf("%s선수의 기록 :\n",player[1]);
+		rpt2 =scanner.nextDouble();
+		System.out.printf("%s선수의 기록 :\n",player[2]);
+		rpt3 =scanner.nextDouble();
+		System.out.println("####성적랭킹####");
+		
+		
+		if(rpt1<rpt2 && rpt1<rpt3 && rpt2<rpt3) {
+			System.out.printf("1등: %s선수기록 :(%f초)",player[0],rpt1);
+			System.out.printf("2등: %s선수기록 :(%f초)",player[1],rpt2);
+			System.out.printf("3등: %s선수기록 :(%f초)",player[2],rpt3);
+		}else if(rpt2<rpt1 && rpt2<rpt3 && rpt1>rpt3) {
+			System.out.printf("1등: %s선수기록 :(%f초)",player[1],rpt2);
+			System.out.printf("2등: %s선수기록 :(%f초)",player[2],rpt3);
+			System.out.printf("3등: %s선수기록 :(%f초)",player[0],rpt1);
+		}else if (rpt3<rpt1 && rpt3<rpt2 && rpt1<rpt2) {
+		System.out.printf("1등: %s선수기록 :(%f초)",player[1],rpt2);
+		System.out.printf("1등: %s선수기록 :(%f초)",player[1],rpt2);
+		System.out.printf("1등: %s선수기록 :(%f초)",player[1],rpt2);
 	}
-
+	
+		
+	}
+		
 }
+		
+		
+		
+		
+		
+		
+		
+
